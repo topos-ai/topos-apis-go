142b608e189dd474680de10fadf8b134843386da
Eric Buth <eric@topos.com>
2019-08-26 13:51:36 -0400

    point iterator

--- points/points.go
+++ points/points.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math"
 	"os"
 	"strings"
 
@@ -11,6 +12,7 @@ import (
 	points "github.com/topos-ai/topos-apis/genproto/go/topos/points/v1"
 	geom "github.com/twpayne/go-geom"
 	"github.com/twpayne/go-geom/encoding/geojson"
+	"google.golang.org/api/iterator"
 	"google.golang.org/grpc"
 )
 
@@ -92,47 +94,67 @@ func (c *Client) PolygonCountPoints(ctx context.Context, tags []string, polygon 
 	return response.TagPoints, nil
 }
 
-func (c *Client) PolygonSearchPoints(ctx context.Context, brand string, tags []string, polygon *geom.Polygon) (map[string]int64, error) {
-	client, err := c.pointsClient.PolygonSearchPoints(ctx)
-	if err != nil {
-		return nil, err
-	}
+func (c *Client) PolygonSearchPoints(ctx context.Context, brand string, tags []string, polygon *geom.Polygon) (*PointIterator, error) {
 
 	encodedGeometry, err := geojson.Marshal(polygon)
 	if err != nil {
 		return nil, err
 	}
 
-	req := &points.PolygonSearchPointsRequest{
-		Brand:           brand,
-		Tags:            tags,
-		PolygonEncoding: points.PolygonEncoding_GEOJSON,
-		PageSize:        10,
-	}
+	it := &PointIterator{}
+	fetch := func(pageSize int, pageToken string) (string, error) {
+		client, err := c.pointsClient.PolygonSearchPoints(ctx)
+		if err != nil {
+			return "", err
+		}
 
-	for chunk := encodedGeometry; chunk != nil; {
-		if len(chunk) > 1024 {
-			req.PolygonChunk = chunk[:1024]
-			chunk = chunk[1024:]
+		req := &points.PolygonSearchPointsRequest{
+			Brand:           brand,
+			Tags:            tags,
+			PolygonEncoding: points.PolygonEncoding_GEOJSON,
+
+			PageToken: pageToken,
+		}
+
+		if pageSize > math.MaxInt32 {
+			req.PageSize = math.MaxInt32
 		} else {
-			req.PolygonChunk = chunk
-			chunk = nil
+			req.PageSize = int32(pageSize)
 		}
 
-		if err := client.Send(req); err != nil {
-			return nil, err
+		for chunk := encodedGeometry; chunk != nil; {
+			if len(chunk) > 1024 {
+				req.PolygonChunk = chunk[:1024]
+				chunk = chunk[1024:]
+			} else {
+				req.PolygonChunk = chunk
+				chunk = nil
+			}
+
+			if err := client.Send(req); err != nil {
+				return "", err
+			}
+
+			*req = points.PolygonSearchPointsRequest{}
 		}
+
+		response, err := client.CloseAndRecv()
+		if err != nil {
+			return "", err
+		}
+
+		if it.items == nil {
+			it.items = response.Points
+		} else {
+			it.items = append(it.items, response.Points...)
+		}
+
+		return response.NextPageToken, nil
 	}
 
-	response, err := client.CloseAndRecv()
-	if err != nil {
-		return nil, err
-	}
-
-	json.NewEncoder(os.Stdout).Encode(response)
-	fmt.Println(response)
-
-	return nil, nil
+	it.pageInfo, it.nextFunc = iterator.NewPageInfo(fetch, it.bufLen, it.takeBuf)
+	it.pageInfo.MaxSize = 1024
+	return it, nil
 }
 
 func (c *Client) GetBrand(ctx context.Context, brand string) (*points.Brand, error) {
@@ -141,4 +163,40 @@ func (c *Client) GetBrand(ctx context.Context, brand string) (*points.Brand, err
 	}
 
 	return c.pointsClient.GetBrand(ctx, req)
+}
+
+type PointIterator struct {
+	items    []*points.Point
+	pageInfo *iterator.PageInfo
+	nextFunc func() error
+}
+
+// PageInfo supports pagination. See the google.golang.org/api/iterator package
+// for details.
+func (it *PointIterator) PageInfo() *iterator.PageInfo {
+	return it.pageInfo
+}
+
+// Next returns the next result. Its second return value is iterator.Done if
+// there are no more results. Once Next returns Done, all subsequent calls will
+// return Done.
+func (it *PointIterator) Next() (*points.Point, error) {
+	var item *points.Point
+	if err := it.nextFunc(); err != nil {
+		return item, err
+	}
+
+	item = it.items[0]
+	it.items = it.items[1:]
+	return item, nil
+}
+
+func (it *PointIterator) bufLen() int {
+	return len(it.items)
+}
+
+func (it *PointIterator) takeBuf() interface{} {
+	b := it.items
+	it.items = nil
+	return b
 }
