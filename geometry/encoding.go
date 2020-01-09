137810d694553221923a8d55d6394ba587f097fb
Eric Buth <eric@topos.com>
2020-01-09 17:23:10 -0500

    marshal and unmarshal geometry, dropped send and recv

--- geometry/encoding.go
+++ geometry/encoding.go
@@ -1,10 +1,8 @@
 package geometry
 
 import (
-	"bytes"
 	"encoding/binary"
 	"encoding/json"
-	"io"
 
 	"github.com/topos-ai/topos-apis/genproto/go/topos/geometry"
 	geom "github.com/twpayne/go-geom"
@@ -14,11 +12,21 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func decodeGeometry(data []byte, encoding geometry.Encoding) (geom.T, error) {
+func Marshal(geometryObject geom.T, encoding geometry.Encoding) ([]byte, error) {
+	switch encoding {
+	case geometry.Encoding_WKB:
+		return wkb.Marshal(geometryObject, binary.BigEndian)
+	case geometry.Encoding_GEOJSON:
+		return geojson.Marshal(geometryObject)
+	default:
+		return nil, status.Error(codes.InvalidArgument, "unknown geometry encoding")
+	}
+}
+
+func Unmarshal(data []byte, encoding geometry.Encoding) (geom.T, error) {
 	switch encoding {
 	case geometry.Encoding_WKB:
 		return wkb.Unmarshal(data)
-
 	case geometry.Encoding_GEOJSON:
 		gg := &geojson.Geometry{}
 		if err := json.Unmarshal(data, gg); err != nil {
@@ -30,99 +38,4 @@ func decodeGeometry(data []byte, encoding geometry.Encoding) (geom.T, error) {
 	default:
 		return nil, status.Error(codes.InvalidArgument, "unknown geometry encoding")
 	}
-}
-
-func RecvGeometry(encoding geometry.Encoding, chunk []byte, next func() ([]byte, error)) (geom.T, error) {
-	if len(chunk) > 1024 {
-		return nil, status.Error(codes.InvalidArgument, "geometry chunk longer than 1024 bytes")
-	}
-
-	buffer := bytes.NewBuffer(make([]byte, 0, len(chunk)))
-	if _, err := buffer.Write(chunk); err != nil {
-		return nil, err
-	}
-
-	for {
-		chunk, err := next()
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			return nil, err
-		}
-
-		if len(chunk) > 1024 {
-			return nil, status.Error(codes.InvalidArgument, "geometry chunk longer than 1024 bytes")
-		}
-
-		if _, err := buffer.Write(chunk); err != nil {
-			return nil, err
-		}
-	}
-
-	return decodeGeometry(buffer.Bytes(), encoding)
-}
-
-func SendGeometry(encoding geometry.Encoding, geometryObject geom.T, send func(chunk []byte) error) error {
-	var chunk []byte
-	switch encoding {
-	case geometry.Encoding_WKB:
-		wkbBytes, err := wkb.Marshal(geometryObject, binary.BigEndian)
-		if err != nil {
-			return err
-		}
-
-		chunk = wkbBytes
-
-	case geometry.Encoding_GEOJSON:
-		geojsonBytes, err := geojson.Marshal(geometryObject)
-		if err != nil {
-			return err
-		}
-
-		chunk = geojsonBytes
-
-	default:
-		return status.Error(codes.InvalidArgument, "unknown geometry encoding")
-	}
-
-	for len(chunk) > 1024 {
-		if err := send(chunk[:1024]); err != nil {
-			return err
-		}
-
-		chunk = chunk[1024:]
-	}
-
-	if len(chunk) > 0 {
-		return send(chunk)
-	}
-
-	return nil
-}
-
-func SendRawGeometry(encoding geometry.Encoding, wkbGeometry []byte, send func(chunk []byte) error) error {
-	if encoding != geometry.Encoding_WKB {
-		geometryObject, err := wkb.Unmarshal(wkbGeometry)
-		if err != nil {
-			return err
-		}
-
-		return SendGeometry(encoding, geometryObject, send)
-	}
-
-	for len(wkbGeometry) > 1024 {
-		if err := send(wkbGeometry[:1024]); err != nil {
-			return err
-		}
-
-		wkbGeometry = wkbGeometry[1024:]
-	}
-
-	if len(wkbGeometry) > 0 {
-		return send(wkbGeometry)
-	}
-
-	return nil
 }
